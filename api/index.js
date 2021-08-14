c8ea23816cf2a39dd1cb6680c52b66e707d76ab2
hilmanski <hilman@hilman.com>
2021-08-14 14:36:42 +0800

    add detabase for prev image cached

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const puppeteer = require('puppeteer');
 const chromium = require('chrome-aws-lambda');
 const ImageKit = require("imagekit");
+const { Deta } = require("deta")
+
+const deta_project_key = process.env.deta_project_key
+const deta = Deta(deta_project_key);
 
 const imagekit = new ImageKit({
     publicKey: process.env.imagekit_public_key,
@@ -9,7 +13,26 @@ const imagekit = new ImageKit({
     urlEndpoint: process.env.imagekit_endpoint,
 });
 
-(async () => {
+//Usage end:
+//get.thumbnailed.com?web=hilman.space&title=life%20is%20good
+
+module.exports = async(req, res) => {
+    const { web, title } = req.query;
+
+    const filename = title //must be dynamic depend on host
+    const foldername = web.replace('.','-')
+
+    //Check if exists, no need to create again, just return the value (thumbnail URL)
+    const db = deta.Base("thumbnails")
+    const { items } = await db.fetch({ foldername, filename})
+    
+    if(items.length !== 0) {
+        const item = items[0]
+        return res.redirect(item.url)
+    }
+
+    const ss_source_url = `http://localhost:5500/index.html?title=${title}`
+
     const browser = await puppeteer.launch({ 
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
@@ -18,21 +41,24 @@ const imagekit = new ImageKit({
         ignoreHTTPSErrors: true,
     })
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.setViewport({ width: 500, height: 300 })
+    await page.goto(ss_source_url);
 
     const options = { encoding: 'binary', type: 'png' };
     const imgBuffer = await page.screenshot(options);
 
-    const filename = "vercel.jpg"
+    await browser.close();
 
     imagekit.upload({
+        folder: foldername,
         file: imgBuffer, 
         fileName: filename,   
-    }).then(response => {
-        console.log(response);
-    }).catch(error => {
-        console.log(error);
-    });
+    }).then(async function(response){
+        //save to db
+        const uploadToDeta = await db.put({ foldername, filename, url: response.url})
+        return res.redirect(uploadToDeta.url)
 
-    await browser.close();
-})();
+    }).catch(error => {
+        return res.json({ body: error })
+    });
+}
