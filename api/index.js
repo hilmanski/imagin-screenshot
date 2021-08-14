c152da37e65f3d117cf597db9cd7794f9a6e6afe
hilmanski <hilman@hilman.com>
2021-08-14 13:28:35 +0800

    no sandbox for linux

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const imagekit = new ImageKit({
 });
 
 (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox'] })
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
     const page = await browser.newPage();
     await page.goto('https://example.com');
 
